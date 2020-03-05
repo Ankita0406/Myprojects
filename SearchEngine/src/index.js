903e805d938b7509fbd306b3a785438552717aed
Ankita <ankitabisht.rajput@gmail.com>
2020-03-05 20:48:54 +0530

    Project data files

---
+++ SearchEngine/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+ 
+
+
+ReactDOM.render(<App message = "comp from app " /> , document.querySelector("#root"));
